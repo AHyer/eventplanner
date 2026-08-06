911466d4ec3ba2b59fb3c647ef32c73582ad2c20
Anna Hyer <anna.hyer@wsu.edu>
2026-08-05 21:51:47 -0700

    KAN-46 fix type mismatch in app/dashboard/page.tsx

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,12 +1,3 @@
-// export default function CalendarPage() {  // function = component ;  returns UI elements
-//   return (
-//     // <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-//     <div>
-//       <main className="calendar-background flex flex-1 w-full flex-col items-center justify-between py-66 px-16 bg-white dark:bg-black "> </main>
-//     </div>
-//   );
-// }
-
 // app/dashboard/page.tsx
 "use client";
 
